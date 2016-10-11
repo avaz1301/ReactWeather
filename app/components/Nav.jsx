20cdf3c36f6221ad273abd2716f008174b2fa45b
Angelo Zamudio <avaz1301@gmail.com>
2016-10-11 16:06:01 -0400

    Update middleware for localhost

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,9 +18,9 @@ var Nav = (props) =>{
   return(
     <div>
       <h3>Nav Component</h3>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >Get Weather </IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get About </Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Examples </Link>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Get Weather </IndexLink>
+      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> About </Link>
+      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Examples </Link>
     </div>
   );
 }
